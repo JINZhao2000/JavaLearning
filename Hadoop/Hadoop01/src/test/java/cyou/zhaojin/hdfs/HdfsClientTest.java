e6ca37c470e17ecddd808e02fe2f75642588a689
JIN Zhao <jinzhaofr@outlook.com>
2021-08-10 23:25:31 +0200

    hadoop

--- Hadoop/Hadoop01/src/test/java/cyou/zhaojin/hdfs/HdfsClientTest.java
+++ Hadoop/Hadoop01/src/test/java/cyou/zhaojin/hdfs/HdfsClientTest.java
@@ -1,14 +1,14 @@
 package cyou.zhaojin.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Arrays;
 
 /**
  * @ Description
@@ -28,7 +28,7 @@ public class HdfsClientTest {
     @Test
     public void uploadTest() throws Exception {
         // delete Y/N - overwrite Y/N - src Path - dest Path
-        fs.copyFromLocalFile(false, false,
+        fs.copyFromLocalFile(false, true,
                 new Path(HdfsClientTest.class.getClassLoader().getResource("log4j.properties").toURI().toString()),
                 new Path("hdfs://hadoop01/uploaded/log4j.properties"));
     }
@@ -41,6 +41,50 @@ public class HdfsClientTest {
                 new Path("hdfs://hadoop01/uploaded/log4j.properties"),
                 dest, false);
         System.out.println(dest.toString());
+    }
+
+    @Test
+    public void testRm() throws Exception {
+        // path - recursive (rm -r)
+        fs.delete(new Path("hdfs://hadoop01/output"), true);
+    }
+
+    @Test
+    public void testMv() throws Exception {
+        // src Path - dest Path
+        fs.rename(new Path("/input/word.txt"), new Path("/input/word2.txt"));
+    }
+
+    @Test
+    public void testFileDetail() throws Exception {
+        // path - recursive
+        RemoteIterator<LocatedFileStatus> files = fs.listFiles(new Path("/tmp/"), true);
+        while (files.hasNext()) {
+            LocatedFileStatus next = files.next();
+            System.out.println(next.getPath().toString());
+            System.out.println(next.getPermission().toString());
+            System.out.println(next.getOwner());
+            System.out.println(next.getGroup());
+            System.out.println(next.getLen());
+            System.out.println(next.getModificationTime());
+            System.out.println(next.getReplication());
+            System.out.println(next.getBlockSize());
+            System.out.println(Arrays.toString(next.getBlockLocations()));
+            System.out.println(next.getPath().getName());
+            System.out.println("==================");
+        }
+    }
+
+    @Test
+    public void testFileDirectory() throws Exception {
+        FileStatus[] fileStatuses = fs.listStatus(new Path("/"));
+        for (FileStatus fileStatus : fileStatuses) {
+            if (fileStatus.isFile()) {
+                System.out.println(fileStatus.getPath().getName() + " is a file");
+            } else {
+                System.out.println(fileStatus.getPath().getName() + " is a directory");
+            }
+        }
     }
 
     /**
